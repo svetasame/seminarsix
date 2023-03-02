cf26335fd799eadebbd78968886b81d497f74949
Svetlana Kovalenko <milkshake9273@gmail.com>
2023-03-02 15:09:58 +1000

    положительные числа

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,4 +1,4 @@
-﻿int [] Prompt(string message)
+﻿int [] PromptArray(string message)
 {
 System.Console.WriteLine(message);
 int[] arraydigits = Array.ConvertAll(Console.ReadLine().Split(','), digit => {return System.Convert.ToInt32 (digit);});
@@ -16,6 +16,7 @@ void CheckArray (int [] array)
   System.Console.WriteLine(count);
 }
 
+
 void PrintArray (int [] array)
 {
   System.Console.WriteLine(String.Join(" ", array));
@@ -23,5 +24,26 @@ void PrintArray (int [] array)
 
 
 // задача 41 пользователь ввел массив чисел, нужно посчитать сколько было введено чисел больш 0
-int [] array = Prompt("введите массив");
-CheckArray (array);
+// int [] array = PromptArray("введите массив");
+// CheckArray (array);
+
+//программа которая найдет точку пересечения 2 прямых заданных уравнениями 
+// y=k1*x+b1, y = k2*x+b2
+void Linec
+
+
+
+int [] array = PromptArray("введите b1,k1,b2,k2:");
+PrintArray (array);
+
+
+int n = 0;
+void Count (ref int Count)
+{
+  System.Console.WriteLine(n);
+  Count++;
+}
+
+int number = 3; 
+Count (ref number);
+System.Console.WriteLine(number);
