e75c9a9f25acfef59e1b8ca1faa1f88482c86e41
Svetlana Kovalenko <milkshake9273@gmail.com>
2023-03-02 14:42:52 +1000

    положительные числа

---
+++ homework/Program.cs
@@ -0,0 +1,27 @@
+﻿int [] Prompt(string message)
+{
+System.Console.WriteLine(message);
+int[] arraydigits = Array.ConvertAll(Console.ReadLine().Split(','), digit => {return System.Convert.ToInt32 (digit);});
+return arraydigits;
+}
+
+void CheckArray (int [] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0)
+    count++;
+  }
+  System.Console.WriteLine(count);
+}
+
+void PrintArray (int [] array)
+{
+  System.Console.WriteLine(String.Join(" ", array));
+}
+
+
+// задача 41 пользователь ввел массив чисел, нужно посчитать сколько было введено чисел больш 0
+int [] array = Prompt("введите массив");
+CheckArray (array);
